cd81d1510acbaa8241b78abfd76d1ce9bbe33d7d
Toru Takahashi <4136375+toru-takahashi@users.noreply.github.com>
2023-07-05 13:48:17 +0900

    update to the latest

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -1,77 +1,86 @@
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 
-let ENV_FILE_NAME = '';
+let ENV_FILE_NAME = "";
 switch (process.env.NODE_ENV) {
-  case 'production':
-    ENV_FILE_NAME = '.env.production';
+  case "production":
+    ENV_FILE_NAME = ".env.production";
     break;
-  case 'staging':
-    ENV_FILE_NAME = '.env.staging';
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
     break;
-  case 'test':
-    ENV_FILE_NAME = '.env.test';
+  case "test":
+    ENV_FILE_NAME = ".env.test";
     break;
-  case 'development':
+  case "development":
   default:
-    ENV_FILE_NAME = '.env';
+    ENV_FILE_NAME = ".env";
     break;
 }
 
 try {
-  dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
-} catch (e) {
-}
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7700,http://localhost:7701";
+const ADMIN_CORS =
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8100,http://storefront:8100";
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-// Database URL (here we use a local database called medusa-development)
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 
-// Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
-// Stripe keys
-const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
-const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
-
-// This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
-  // Minio
   {
-    resolve: `medusa-file-minio`,
+    resolve: `@medusajs/file-local`,
     options: {
-      endpoint: process.env.MINIO_ENDPOINT,
-      bucket: process.env.MINIO_BUCKET,
-      access_key_id: process.env.MINIO_ACCESS_KEY,
-      secret_access_key: process.env.MINIO_SECRET_KEY,
+      upload_dir: "uploads",
     },
   },
-  // Uncomment to add Stripe support.
-  // You can create a Stripe account via: https://stripe.com
+  // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
   // {
-  //   resolve: `medusa-payment-stripe`,
+  //   resolve: "@medusajs/admin",
+  //   /** @type {import('@medusajs/admin').PluginOptions} */
   //   options: {
-  //     api_key: STRIPE_API_KEY,
-  //     webhook_secret: STRIPE_WEBHOOK_SECRET,
+  //     autoRebuild: true,
   //   },
   // },
 ];
 
-module.exports = {
-  projectConfig: {
-    redis_url: REDIS_URL,
-    // For more production-like environment install PostgresQL
-    database_url: DATABASE_URL,
-    database_type: "postgres",
-    store_cors: STORE_CORS,
-    admin_cors: ADMIN_CORS,
+const modules = {
+  /*eventBus: {
+    resolve: "@medusajs/event-bus-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
   },
+  cacheService: {
+    resolve: "@medusajs/cache-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },*/
+};
+
+/** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
+const projectConfig = {
+  jwtSecret: process.env.JWT_SECRET,
+  cookieSecret: process.env.COOKIE_SECRET,
+  store_cors: STORE_CORS,
+  database_url: DATABASE_URL,
+  admin_cors: ADMIN_CORS,
+  // Uncomment the following lines to enable REDIS
+  // redis_url: REDIS_URL
+};
+
+/** @type {import('@medusajs/medusa').ConfigModule} */
+module.exports = {
+  projectConfig,
   plugins,
+  modules,
 };
