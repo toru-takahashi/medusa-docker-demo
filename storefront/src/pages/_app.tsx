f87101d4dd789b5326cd2599de2c5272ba799cb2
Toru Takahashi <4136375+toru-takahashi@users.noreply.github.com>
2023-07-05 16:38:35 +0900

    Add TD JS SDK tracking code

--- storefront/src/pages/_app.tsx
+++ storefront/src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { CartProvider, MedusaProvider } from "medusa-react"
 import { Hydrate } from "react-query"
 import "styles/globals.css"
 import { AppPropsWithLayout } from "types/global"
+import Script from "next/script"
 
 function App({
   Component,
@@ -22,6 +23,30 @@ function App({
       }}
     >
       <Hydrate state={pageProps.dehydratedState}>
+
+      <>
+<Script id="td_js_load">
+{`
+!function(t,e){if(void 0===e[t]){e[t]=function(){e[t].clients.push(this),this._init=[Array.prototype.slice.call(arguments)]},e[t].clients=[];for(var r=["addRecord","blockEvents","fetchServerCookie","fetchGlobalID","fetchUserSegments","resetUUID","ready","setSignedMode","setAnonymousMode","set","trackEvent","trackPageview","trackClicks","unblockEvents"],s=0;s<r.length;s++){var c=r[s];e[t].prototype[c]=function(t){return function(){return this["_"+t]=this["_"+t]||[],this["_"+t].push(Array.prototype.slice.call(arguments)),this}}(c)}var n=document.createElement("script"),o=(n.type="text/javascript",n.async=!0,n.src=("https:"===document.location.protocol?"https:":"http:")+"//cdn.treasuredata.com/sdk/4.0/td.min.js",document.getElementsByTagName("script")[0]);o.parentNode.insertBefore(n,o)}}("Treasure",this);
+`}
+</Script>
+
+<Script strategy="afterInteractive"  id="td_analytics">
+{`
+var td = new Treasure({
+  host: 'API Endpoint',
+  writeKey: 'TD_API_KEY',
+  database: 'Your_DB'
+});
+td.setSignedMode();
+td.set('$global', 'td_global_id', 'td_global_id');
+td.trackPageview('pageviews');
+td.trackClicks('clicks');
+`}
+</Script>
+</>
+
+
         <CartDropdownProvider>
           <MobileMenuProvider>
             <CartProvider>
