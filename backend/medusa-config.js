e660f739bdbeafbdbeeb5589191d9b138a01ad3e
Toru Takahashi <4136375+toru-takahashi@users.noreply.github.com>
2023-07-05 14:26:38 +0900

    Fix CORS for Admin

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -23,7 +23,7 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+  process.env.ADMIN_CORS || "http://localhost:7700";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8100";
